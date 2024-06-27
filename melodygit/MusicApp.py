e5e3462a2c782d530cc36c86ac0c74d9247c8d3f
ilbe555 <165358852+ilbe555@users.noreply.github.com>
2024-06-27 03:15:32 +0300

    Add files via upload

---
+++ melodygit/MusicApp.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+import pygame
+import time
+
+
+class MelodyEditor:
+
+class MelodyApp(Tk):
+
+class Tutorial(Tk):
+
+class Main(Tk):
+
+if __name__ == "__main__":
+    Main().mainloop()
